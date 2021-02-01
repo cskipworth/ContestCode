12e599f56e119b3f26f6100f218f8cc2d012a353
Clarissa Skipworth <68129345+cskipworth@users.noreply.github.com>
2021-01-31 23:48:47 -0800

    Update DSArrayList.java

--- DSArrayList.java
+++ DSArrayList.java
@@ -34,7 +34,5 @@ public class DSArrayList<E> {
         a = (E[]) (new Object[initial_size]);
         this.setLength(0);
     }
-
-    //Method that adds a value onto end of current items in DSArrayList.
      
  }
